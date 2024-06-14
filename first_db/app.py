9705d0c2ba2065274238b1e1b935f805773664ac
Liew Chooi Chin <127520432+liewchooichin@users.noreply.github.com>
2024-06-14 09:18:01 +0000

    fast api

--- first_db/app.py
+++ first_db/app.py
@@ -92,7 +92,7 @@ def create_person(item: Person):
 
 def get_conn():
     credential = identity.DefaultAzureCredential(exclude_interactive_browser_credential=False)
-    token_bytes = credential.get_token("https://sqllcc200/database.windows.net/.default").token.encode("UTF-16-LE")
+    token_bytes = credential.get_token("https://sqllcc200.database.windows.net/.default").token.encode("UTF-16-LE")
     token_struct = struct.pack(f'<I{len(token_bytes)}s', len(token_bytes), token_bytes)
     SQL_COPT_SS_ACCESS_TOKEN = 1256  # This connection option is defined by microsoft in msodbcsql.h
     conn = pyodbc.connect(connection_string, attrs_before={SQL_COPT_SS_ACCESS_TOKEN: token_struct})
